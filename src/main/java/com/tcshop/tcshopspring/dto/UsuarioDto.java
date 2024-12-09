ea0920e884229cfc24097d8935250cd2bf6c6265
Paulozzq <paulo.garfar@gmail.com>
2024-12-08 19:15:50 -0500

    first commit

--- src/main/java/com/tcshop/tcshopspring/dto/UsuarioDto.java
+++ src/main/java/com/tcshop/tcshopspring/dto/UsuarioDto.java
@@ -1,19 +1,84 @@
 package com.tcshop.tcshopspring.dto;
 
+import com.tcshop.tcshopspring.modelo.entidades.Carrera;
+import com.tcshop.tcshopspring.modelo.entidades.Departamento;
+import com.tcshop.tcshopspring.modelo.entidades.Rol;
+import com.tcshop.tcshopspring.modelo.entidades.Sede;
+
 public class UsuarioDto {
     private Integer id;
     private String name;
     private String username;
     private String email;
+    private Rol rol;
+    private String number;
+    private Boolean enable;
+    private Sede sede;
+    private Departamento departamento;
+    private Carrera carrera;
 
     public UsuarioDto() {
     }
 
-    public UsuarioDto(Integer id, String name, String username, String email) {
+    public UsuarioDto(Integer id, String name, String username, String email, Boolean enable, String number, Rol rol, Sede sede, Departamento departamento, Carrera carrera) {
         this.id = id;
         this.name = name;
         this.username = username;
         this.email = email;
+        this.enable = enable;
+        this.number = number;
+        this.rol = rol;
+        this.sede = sede;
+        this.departamento = departamento;
+        this.carrera = carrera;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    public Boolean getEnable() {
+        return enable;
+    }
+
+    public void setEnable(Boolean enable) {
+        this.enable = enable;
+    }
+
+    public Sede getSede() {
+        return sede;
+    }
+
+    public void setSede(Sede sede) {
+        this.sede = sede;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    public Carrera getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(Carrera carrera) {
+        this.carrera = carrera;
     }
 
     public Integer getId() {
